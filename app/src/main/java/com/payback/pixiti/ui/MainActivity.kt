0a8ed20819ddd86e54e391d5772f8cd1dc5e98ce
M.Emre YILMAZ <memreyilmaz@gmail.com>
2021-04-02 11:12:03 +0300

    Downgraded Material, navigation and lifecycle versions to proper ones Created ImageListFragment and ImageDetailFragment Created navigation.xml file

--- app/src/main/java/com/payback/pixiti/ui/MainActivity.kt
+++ app/src/main/java/com/payback/pixiti/ui/MainActivity.kt
@@ -1,11 +1,13 @@
-package com.payback.pixiti
+package com.payback.pixiti.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.payback.pixiti.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
