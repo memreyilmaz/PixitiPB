a5459a9a585ac48407d3271653da49f0da5ebf9b
M.Emre YILMAZ <memreyilmaz@gmail.com>
2021-04-05 10:21:27 +0300

    Reorganized DialogFragment and AlertDialog styles and colors

--- app/src/main/java/com/payback/pixiti/utils/ContextExt.kt
+++ app/src/main/java/com/payback/pixiti/utils/ContextExt.kt
@@ -2,11 +2,14 @@ package com.payback.pixiti.utils
 
 import android.app.Activity
 import android.content.Context
+import android.graphics.Color
 import android.view.Gravity
 import android.view.View
 import android.view.inputmethod.InputMethodManager
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
+import com.payback.pixiti.R
 
 /*
  * Create an toast
@@ -24,7 +27,7 @@ fun Context.toastForDetail(
         length: Int = Toast.LENGTH_SHORT
 ) {
     val toast = Toast.makeText(this, message, length)
-    toast.setGravity(Gravity.BOTTOM, 0 , 200)
+    toast.setGravity(Gravity.BOTTOM, 0, 200)
     toast.show()
 }
 
@@ -41,10 +44,18 @@ fun Context.showAlertDialog(
         positiveButtonListener: () -> Unit
 ) =
         run {
-            val builder = AlertDialog.Builder(this)
+            val builder = AlertDialog.Builder(this, R.style.AlertDialog)
             builder.apply {
                 title?.let {
-                    setTitle(it)
+                    //Using custom TextView for long titles more than 2 lines
+                    val titleTextView = TextView(context)
+                    titleTextView.apply {
+                        text = title
+                        setPadding(60,40,60,20)
+                        setTextColor(Color.WHITE)
+                        textSize = 18.0f
+                    }
+                    setCustomTitle(titleTextView)
                 }
                 setMessage(message)
             }
